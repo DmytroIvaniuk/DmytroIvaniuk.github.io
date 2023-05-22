f87a806e730218a3aa51ea581010b15f59e3f7b6
DmytroIvaniuk <dmytro.ivaniuk55@gmail.com>
2023-05-22 03:22:23 +0300

    wrote HTML for CV template

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-function showFormData() {
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let phoneNumber = document.getElementById("phonenum").value;
-    let message = document.getElementById("message").value;
-
-    alert(`Name: ${name}\nEmail: ${email}\nPhone Number: ${phoneNumber}\nMessage: ${message}`);
-}
